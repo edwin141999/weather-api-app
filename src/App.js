85000f390e6175074aa9344b2bc91767bf8fc756
edwin141999 <183433@ids.upchiapas.edu.mx>
2022-02-22 00:22:33 -0600

    change css and start api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [city, setCity] = useState("")
+  const key = 'bb8b7a1351ef4ccb95b13932222202'
+
+  const linkAPI = `https://api.weatherapi.com/v1/current.json?key=${key}&q=${city}&aqi=no`
+
+  const weatherCity = (event) => {
+    event.preventDefault()
+    fetch(linkAPI)
+      .then(res => res.json())
+      .then(response => {
+        console.log(response);
+      })
+      .catch((e) => {
+        console.log(e);
+      })
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section>
+        <form>
+          <input
+            placeholder='city name'
+            type='text'
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+          ></input>
+          <button onClick={weatherCity}>Check</button>
+        </form>
+      </section>
     </div>
   );
 }
