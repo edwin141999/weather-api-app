d334d71e770573693a23d252962a347ad99f88f8
edwin141999 <183433@ids.upchiapas.edu.mx>
2022-03-03 19:08:41 -0600

    little changes in Tailwind

--- src/components/Current.js
+++ src/components/Current.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import ErrorPage from './ErrorPage';
+import LoadingPage from './LoadingPage';
 
 function Current() {
 
@@ -14,29 +16,27 @@ function Current() {
 
     const weatherCity = (city) => {
         const key = 'bb8b7a1351ef4ccb95b13932222202'
-        const linkAPI = `http://api.weatherapi.com/v1/forecast.json?key=${key}&q=${city}&days=7`
+        const linkAPI = `http://api.weatherapi.com/v1/forecast.json?key=${key}&q=${city}&days=7&lang=es`
         setLoading(true)
-        if (city !== "") {
-            setTimeout(() => {
-                fetch(linkAPI)
-                    .then(res => res.json())
-                    .then(response => {
-                        console.log(response);
-                        if (response.error) {
-                            setErrorMessage(response.error.message)
-                        } else {
-                            setErrorMessage("")
-                            setLocation(response.location)
-                            setCurrent(response.current)
-                            setCondition(response.current.condition)
-                        }
-                        setLoading(false)
-                    })
-                    .catch((e) => {
-                        console.log(e);
-                    })
-            }, 2000)
-        }
+        setTimeout(() => {
+            fetch(linkAPI)
+                .then(res => res.json())
+                .then(response => {
+                    console.log(response);
+                    if (response.error) {
+                        setErrorMessage(response.error.message)
+                    } else {
+                        setErrorMessage("")
+                        setLocation(response.location)
+                        setCurrent(response.current)
+                        setCondition(response.current.condition)
+                    }
+                    setLoading(false)
+                })
+                .catch((e) => {
+                    console.log(e);
+                })
+        }, 2000)
     }
 
     useEffect(() => {
@@ -47,15 +47,13 @@ function Current() {
     }, [state])
 
     const styles = {
-        bold: `text-gray-700 text-base font-bold`,
+        bold: `text-gray-700 text-xs sm:text-xs md:text-base lg:text-base font-bold`,
         semibold: `font-semibold`
     }
 
     if (location.length === 0) {
         if (errorMessage === 'No matching location found.') {
-            message = <div className='p-5 bg-red-600 rounded'>
-                <p className='font-semibold text-white'>{errorMessage}</p>
-            </div>
+            message = <ErrorPage errorMessage={errorMessage} />
         } else {
             message = <div className='p-5 bg-blue-500 rounded'>
                 <p className='font-semibold text-white'>Waiting for a city</p>
@@ -64,21 +62,17 @@ function Current() {
     }
     else {
         if (errorMessage === 'No matching location found.') {
-            message =
-                <div className='p-5 bg-red-600'>
-                    <p className='font-semibold text-white'>{errorMessage}</p>
-                </div>
+            message = <ErrorPage errorMessage={errorMessage} />
         } else {
-            console.log('size:', location.length);
             message = <>
-                <p className='text-2xl font-bold text-gray-700'>Clima en {location.name}</p>
-                <span className='text-2xl font-semibold text-gray-700'>{current.temp_c}°C</span>
-                <div className='flex items-center justify-start'>
-                    <img src={condition.icon} alt='No hay imagen' className='px-5'></img>
+                <p className='text-base sm:text-xs md:text-2xl lg:text-2xl font-bold text-gray-700'>Clima en {location.name}</p>
+                <span className='text-base sm:text-xs md:text-2xl lg:text-2xl font-semibold text-gray-700'>{current.temp_c}°C</span>
+                <div className='text-center'>
+                    <img src={condition.icon} alt='No hay imagen' className='px-5 mx-auto'></img>
                     <span className='font-semibold text-gray-700'>{condition.text}</span>
                 </div>
                 <p className={styles.bold}>Humedad: <span className={styles.semibold}>{current.humidity}%</span></p>
-                <p className={styles.bold}>Viento: <span className={styles.semibold}>{current.wind_kph} km/h</span></p>
+                <p className={styles.bold}>Velocidad de viento: <span className={styles.semibold}>{current.wind_kph} km/h</span></p>
             </>
         }
     }
@@ -91,7 +85,7 @@ function Current() {
                     {
                         loading
                             ?
-                            <div className='p-5 bg-green-600 rounded-md'><p className='font-semibold text-white'>Cargando...</p></div>
+                            <LoadingPage />
                             :
                             message
                     }
